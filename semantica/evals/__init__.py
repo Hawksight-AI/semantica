0f905a758837a85a0c0a60d99f37480b86491e58
pkupt <zhangqian_zone@sina.com>
2026-08-19 12:32:28 +0800

    feat(evals): add exact/regex/range/length evaluators

--- semantica/evals/__init__.py
+++ semantica/evals/__init__.py
@@ -4,6 +4,8 @@ Semantica Evals Module
 Coming Soon
 """
 
+from . import evaluators  # noqa: F401  import runs register() side effects
+
 __version__ = "0.1.1"
 __status__ = "coming_soon"
 __all__ = []
